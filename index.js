70a208ca45a5f1c1a41649f27b33f8b25eb40129
devburak <dev.burak@gmail.com>
2023-06-17 13:38:43 +0300

    fix: Prevent CORS error

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const connectToDatabase = require('./db');
 //routes
@@ -8,19 +9,21 @@ const administrationRoutes = require('./routes/userAdministration')
 const fileRoutes = require('./routes/file')
 const app = express();
 app.use(express.json());
-// Bağlantıyı yapmak 
+// CORS OK
+app.use(cors());
+// 
 connectToDatabase();
 
 // Temel bir GET 
 app.get('/', (req, res) => {
     res.send('Hello word');
   });
-
 // Routes
 app.use('/api/user', userRoutes);
 app.use('/api/profile',profileRoutes);
 app.use('/api/administration',administrationRoutes);
 app.use('/api/file' , fileRoutes);
+
   const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server http://127.0.0.1:${port} portunda çalışıyor.`);
